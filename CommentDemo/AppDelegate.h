377c50f910a2287395f9816b2be54a0abbed808b
liuwei <liuwei@liuweideMacBook-Pro.local>
2017-05-09 11:11:05 +0800

    Initial Commit

---
+++ CommentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommentDemo
+//
+//  Created by liuwei on 2017/5/9.
+//  Copyright © 2017年 liuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommentDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CommentDemo
+//
+//  Created by liuwei on 2017/5/9.
+//  Copyright © 2017年 liuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
